c1ae93469d96c076cd91d39f5c9f7ae24777e6ea
gabrieljesus18 <gabrielbate759@gmail.com>
2026-03-10 22:26:04 -0300

    primeiro commit interface DevBills

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Button from "../components/Button"
+
+const Home = () => {
+ return (
+    <div>
+      <h1>Home</h1>
+      <Button variant="primary" isLoading>Clique aqui</Button>
+    </div>
+ )
+}
+
+export default Home
