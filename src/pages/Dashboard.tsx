5232c9a433c4b28796f4828a5b705edad2f11b68
gabrieljesus18 <gabrielbate759@gmail.com>
2026-03-18 01:19:06 -0300

    Conectando a API com o FRONTEND

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { api } from "../services/api"
+
 const Dashboard = () => {
+  useEffect(() => {
+
+    async function getTransactions() {
+      const response = await api.get('/transactions')
+      console.log(response)
+    }
+
+    getTransactions()
+  }, [])
+
+
   return (
     <div>
       <h1>Dashboard</h1>
