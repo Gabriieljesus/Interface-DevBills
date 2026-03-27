0733e1483bb1856de6e371f013c028741833ceac
Gabriel de Jesus <gabrielbate759@gmail.com>
2026-03-27 15:06:16 -0300

    Criando a tela de DashBoard o seletor de mês e ano

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../services/api"
+import MonthYearSelect from "../components/MonthYearSelect";
 
 const Dashboard = () => {
+  const currentDate = new Date();
+  const [year, setYear] = useState<number>(currentDate.getFullYear());
+
+  const [month, setMonth] = useState(currentDate.getMonth() + 1);
+
   useEffect(() => {
 
     async function getTransactions() {
       const response = await api.get('/transactions')
-      
+
       console.log(response)
     }
 
@@ -15,10 +21,17 @@ const Dashboard = () => {
 
 
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="container-app py-6">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
+        <h1 className="text-2xl font-bold mb-4 md:mb-8">Dashboard</h1>
+        <MonthYearSelect 
+          month={month} 
+          year={year} 
+          onMonthChange={setMonth} 
+          onYearChange={setYear} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
