52cd3c9c5a65db16278cd46fb633d452ddcc9ef0
Gabriel de Jesus <gabrielbate759@gmail.com>
2026-03-30 13:53:24 -0300

    Criando parte de Resumo

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
-
 import MonthYearSelect from "../components/MonthYearSelect";
-import { getTransactions,  } from "../services/transactionService";
+import { getTransactionsSummary,  } from "../services/transactionService";
+import type { TransactionSummary } from "../types/transactions";
+import Card from "../components/Card";
+import { AArrowUp } from "lucide-react";
+
+const initialSummary: TransactionSummary = {
+  balance: 0,
+  totalExpenses: 0,
+  totalIncomes: 0,
+  expenseByCategory: []
+}
 
 const Dashboard = () => {
   const currentDate = new Date();
   const [year, setYear] = useState<number>(currentDate.getFullYear());
-
   const [month, setMonth] = useState(currentDate.getMonth() + 1);
+  const [summary, setSummary] = useState<TransactionSummary>(initialSummary);
 
   useEffect(() => {
+    async function loadTransactionsSummary() {
+      const response = await getTransactionsSummary(month, year);
 
-    async function getTransactionsUser() {
-      const response = await getTransactions({categoryId: "69a8994e4f766f14f83a6968"});
-
-      console.log(response)
+      setSummary(response)
     }
 
-    getTransactionsUser()
-  }, [])
+    loadTransactionsSummary();
+  }, [month, year])
 
 
   return (
@@ -31,6 +39,17 @@ const Dashboard = () => {
           onMonthChange={setMonth} 
           onYearChange={setYear} />
       </div>
+      <Card 
+        glowEffect 
+        hover 
+        title="Despesas" 
+        subtitle="alguma coisa"
+        icon={<AArrowUp className="text-primary-500" />}
+      >
+        <div>
+          <p className="text-primary-500 font-bold">R$ 2000,00</p>
+        </div>
+      </Card>
     </div>
   );
 };
