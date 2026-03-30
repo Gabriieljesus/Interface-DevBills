11a425dc6193ec027e33ba191287413797ee06d9
Gabriel de Jesus <gabrielbate759@gmail.com>
2026-03-30 16:45:18 -0300

    Finalizando o Card de Resumo

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import MonthYearSelect from "../components/MonthYearSelect";
-import { getTransactionsSummary,  } from "../services/transactionService";
+import { getTransactionsSummary, } from "../services/transactionService";
 import type { TransactionSummary } from "../types/transactions";
 import Card from "../components/Card";
-import { AArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp, Wallet } from "lucide-react";
+import { formtCurrency } from "../utils/formatters";
 
 const initialSummary: TransactionSummary = {
   balance: 0,
@@ -33,23 +34,50 @@ const Dashboard = () => {
     <div className="container-app py-6">
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
         <h1 className="text-2xl font-bold mb-4 md:mb-8">Dashboard</h1>
-        <MonthYearSelect 
-          month={month} 
-          year={year} 
-          onMonthChange={setMonth} 
+        <MonthYearSelect
+          month={month}
+          year={year}
+          onMonthChange={setMonth}
           onYearChange={setYear} />
       </div>
-      <Card 
-        glowEffect 
-        hover 
-        title="Despesas" 
-        subtitle="alguma coisa"
-        icon={<AArrowUp className="text-primary-500" />}
-      >
-        <div>
-          <p className="text-primary-500 font-bold">R$ 2000,00</p>
-        </div>
-      </Card>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <Card
+          icon={<Wallet size={20} className="text-primary-500" />}
+          title="Saldo"
+          hover
+          glowEffect={summary.balance > 0}
+        >
+          <p className={`text-2xl font-semibold mt-2
+            ${summary.balance > 0 ? "text-primary-500" : "text-shadow-red-600"}
+            `}
+          >
+            {formtCurrency(summary.balance)}
+          </p>
+        </Card>
+
+        <Card
+          icon={<ArrowUp size={20} className="text-primary-500" />}
+          title="Receitas"
+          hover
+        >
+          <p className="text-2xl font semibold mt-2 text-primary-500"
+          >
+            {formtCurrency(summary.totalIncomes)}
+          </p>
+        </Card>
+
+        <Card
+          icon={<ArrowDown size={20} className="text-red-600" />}
+          title="Despesas"
+          hover
+        >
+          <p className="text-2xl font semibold mt-2 text-red-600"
+          >
+            {formtCurrency(summary.totalExpenses)}
+          </p>
+        </Card>
+      </div>
     </div>
   );
 };
---
+++ src/utils/formatters.ts
@@ -0,0 +1,6 @@
+export const formtCurrency = (value:number): string => {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+};
