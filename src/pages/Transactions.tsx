75e9d4235e7f2d2082555b35b764e55ceb1d5ef5
Gabriel de Jesus <gabrielbate759@gmail.com>
2026-04-05 17:46:05 -0300

    Continuando Tela de Transações

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,13 +1,37 @@
-import { Plus, Search } from "lucide-react";
+import { AlertCircle, Plus, Search } from "lucide-react";
 import { Link } from "react-router";
 import MonthYearSelect from "../components/MonthYearSelect";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Input from "../components/Input";
+import Card from "../components/Card";
+import type { Transaction } from "../types/transactions";
+import { getTransactions } from "../services/transactionService";
+import Button from "../components/Button";
 
-const Transactions = ( )  => {
+const Transactions = () => {
   const currentDate = new Date();
   const [year, setYear] = useState<number>(currentDate.getFullYear());
   const [month, setMonth] = useState<number>(currentDate.getMonth() + 1);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  const fetchTransactions = async () => {
+    try {
+      setLoading(true);
+      setError("");
+      const data = await getTransactions({ month, year });
+      setTransactions(data);
+    } catch (error) {
+      setError("Não foi possível carregar as transações. Por favor, tente novamente.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, [month, year]);
 
   return (
     <div className="container-app py-6">
@@ -16,29 +40,59 @@ const Transactions = ( )  => {
         <Link to="/transacoes/nova"
           className="bg-primary-500 text-[#051626] font-semibold px-4 py-2.5 rounded-xl flex items-center justify-center hover:bg-primary-500 transition-all"
         >
-          <Plus className="w-4 h-4 mr-2"/> 
+          <Plus className="w-4 h-4 mr-2" />
           Nova Transação
         </Link>
       </div>
 
-      <div className="mb-6">
-        <MonthYearSelect 
-        month={month}
-        year={year}
-        onMonthChange={setMonth} 
-        onYearChange={setYear} 
-       />
-      </div>
+      <Card className="mb-6">
+        <MonthYearSelect
+          month={month}
+          year={year}
+          onMonthChange={setMonth}
+          onYearChange={setYear}
+        />
+      </Card>
 
-      <div className="mb-6">
-        <Input 
+      <Card className="mb-6">
+        <Input
           placeholder="Buscar transações..."
           icon={<Search className="w-4 h-4" />}
           fullWidth
         />
-      </div>
+      </Card>
+
+      <Card className="overflow-hidden">
+        {loading ? (
+          <div className="flex items-center justify-center">
+            <div className="w-10 h-10 border-4 border-primary-500 border-t-transparent rounded-full animate-spin"/>
+          </div>
+        ) : error ? (
+          <div className="p-8 text-center">
+            <AlertCircle className="w-12 h-12 text-red-500 mx-auto mb-4" />
+            <p>{error}</p>
+            <Button onClick={fetchTransactions} className="mx-auto mt-6">
+              Tentar Novamente
+            </Button>
+          </div>
+        ) : transactions?.length === 0 ? (
+          <div className="text-center py-12">
+            <p className="text-gray-500 mb-4">Nenhuma Transação encontrada.</p>
+
+            <Link to="/transacoes/nova"
+              className="w-fit mx-auto mt-6 bg-primary-500 text-[#051626] font-semibold px-4 py-2.5 rounded-xl flex items-center justify-center hover:bg-primary-500 transition-all"
+            >
+              <Plus className="w-4 h-4 mr-2" />
+              Nova Transação
+            </Link>
+
+          </div>
+        ) : (
+          <div>Transações</div>
+        )}
+      </Card>
     </div>
   )
-}
+};
 
 export default Transactions;
