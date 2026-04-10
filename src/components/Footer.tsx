6432b6744defa2c4db788b9ea4d4e48de5d6b7fe
Gabriel de Jesus <gabrielbate759@gmail.com>
2026-04-10 13:19:44 -0300

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="bg-gray-800 border-gray-700 py-4">
+      <div className="container-app">
+        <p className="text-sm text-gray-400 text-center">
+          DevBills {currentYear} - Desenvolvido por <strong>Gabriel de Jesus</strong>. {" "}
+        </p>
+      </div>
+    </footer>
   )
 }
 
